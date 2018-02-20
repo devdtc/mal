6b37c72f4dd4ff931698791ca2582cfe925e9486
Dan <devdtc@gmail.com>
2018-02-19 20:54:20 -0500

    Convert parser to use Parsec instead of Attoparsec

--- haskell2/core/printer.hs
+++ haskell2/core/printer.hs
@@ -13,5 +13,5 @@ printStr :: MalValue -> T.Text
 printStr (MalList vals) = T.concat [T.pack "(", T.intercalate (T.pack " ") (map printStr vals), T.pack ")"]
 printStr (MalSym sym) = sym
 printStr (MalInt int) = T.pack $ show int
-printStr (MalStr str) = str
+printStr (MalStr str) = T.concat [T.pack "\"", str, T.pack "\""]
 
--- haskell2/core/reader.hs
+++ haskell2/core/reader.hs
@@ -6,94 +6,98 @@ where
 
 import Types
 
-import Control.Applicative
-import Data.Attoparsec.Text
-import Data.Text as T
+import Data.Text (Text, pack, unpack)
+
+import Text.Parsec
+import Text.Parsec.Error
+
+type CharParser   = Parsec Text () Char
+type StringParser = Parsec Text () String
+type MalParser    = Parsec Text () MalValue
 
 
---readStr :: Text -> MalValue
---readStr str =
---    case tokenize str of
---        Left err ->  error err
---        Right result -> result
+delim :: CharParser
+delim =
+        space
+    <|> char ','
+
+delims :: StringParser
+delims = many delim
+
+delims1 :: StringParser
+delims1 = many1 delim
+
+malInt :: MalParser
+malInt = do
+    intStr <- pos <|> neg
+
+    pure $ MalInt $ read intStr
+  where
+    pos = many1 digit
+    neg = (:) <$> (char '-') <*> pos
+
+malSymbol :: MalParser
+malSymbol = do
+    symStr <- (:) <$> leadingChar <*> many trailingChar
+
+    pure $ MalSym $ pack symStr
+  where
+    underscore = char '_'
+    specials = oneOf "-+*/"
+
+    leadingChar = letter <|> underscore <|> specials
+    trailingChar = leadingChar <|> digit
+
+malString :: MalParser
+malString = do
+    char '"'
+    strs <- many $ fmap pure nonEscape <|> escape
+    char '"'
+
+    pure $ MalStr $ pack $ concat $ strs
+  where
+    escape = do
+        e <- char '\\'
+        c <- oneOf "\\\""
+        pure [e, c]
+
+    nonEscape = noneOf "\\\""
+
+malAtom :: MalParser
+malAtom =
+        malInt
+    <|> malSymbol
+    <|> malString
+
+malList :: MalParser
+malList = do
+    char '('
+    delims
+
+    vals <- malValue `sepEndBy` delims -- sepEndBy needed if trailing delims allowed
+
+    -- trailing delims handled by above
+    char ')'
+
+    pure $ MalList vals
+
+malValue :: MalParser
+malValue = malList <|> malAtom
+
 
 readStr :: Text -> Either String MalValue
-readStr = tokenize
-
-tokenize :: Text -> Either String MalValue
-tokenize str =
+readStr str =
     case result of
-        Left s -> Left s
-        right@(Right _) -> right
+        Left err -> Left $ concatMap ((++"\n") . messageString) (errorMessages err)
+        Right malVal -> Right malVal
   where
-    delim =
-            space
-        <|> char ','
-
-    delims = many' delim
-    delims1 = many1' delim
-
-    --int = do
-    --    (intStr, _) <- match $ pos <|> neg
-
-    --    return $ MalInt $ read $ unpack intStr
-    --  where
-    --    pos = many1' digit
-    --    neg = do
-    --        char '-'
-    --        pos
-
-    int = fmap (MalInt . fromIntegral) (signed decimal)
-
-    symbol = do
-        (symStr, _) <- match $ do
-            letter <|> underscore <|> specials
-            many' $ letter <|> underscore <|> digit <|> specials
-
-        pure $ MalSym symStr
-      where
-        underscore = char '_'
-        specials = satisfy $ inClass "-+*/"
-
-    strLit = do
-        char '"'
-        --str <- manyTill' anyChar (notChar '\\' <* char '"')
-        --str <- many' $ do
-        --    next <- peekChar
-
-        --    satisfy (\ch -> ch == '\\' || next /= (Just '"'))
-        str <- many' $ qut <|> nc
-        char '"'
-
-        pure $ MalStr $ T.filter (/= '\\') $ T.concat $ str
-      where
-        qut = (string $ pack "\\\"")
-        nc = notChar '"' >>= (\ch -> pure $ singleton ch)
-       
-
-    atom =
-            int
-        <|> symbol
-        <|> strLit
-
-    list = do
-        char '('
-        delims
-        atoms <- value `sepBy` delims1
-        delims
-        char ')'
-
-        pure $ MalList atoms
-
-    value = list <|> atom
-
-    tokenizer :: Parser MalValue
+    tokenizer :: MalParser
     tokenizer = do
         delims
-        val <- value
+        val <- malValue
         delims
 
         pure val
 
-    result = parseOnly (tokenizer <* endOfInput) str
+    result = parse (tokenizer <* eof) "repl" str
 
