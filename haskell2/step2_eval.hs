89e858dd58a592760ab973259a5483197621ba45
Dan <devdtc@gmail.com>
2018-02-23 20:00:28 -0500

    Complete step2

--- haskell2/step2_eval.hs
+++ haskell2/step2_eval.hs
@@ -3,35 +3,72 @@ import Types
 import Reader
 import Printer
 
-import Data.Text
+import Data.Either
+import Data.List.NonEmpty (NonEmpty(..))
+import qualified Data.Map.Strict as M
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
 import System.IO
 
 
-malRead :: Text -> Either String MalValue
+env :: M.Map T.Text MalFunc
+env = M.fromList
+  [ (T.pack "+", \(MalList [MalInt x, MalInt y]) -> MalInt (x + y))
+  , (T.pack "-", \(MalList [MalInt x, MalInt y]) -> MalInt (x - y))
+  , (T.pack "*", \(MalList [MalInt x, MalInt y]) -> MalInt (x * y))
+  , (T.pack "/", \(MalList [MalInt x, MalInt y]) -> MalInt (x `div` y))
+  ]
+
+flattenEithers :: [Either a b] -> Either (NonEmpty a) [b]
+flattenEithers es =
+  case partitionEithers es of
+    (    [], rs) -> Right rs
+    (l : ls, _ ) -> Left $ l :| ls
+
+malRead :: T.Text -> Either String MalValue
 malRead = readStr
 
-malEval :: MalValue -> MalValue
-malEval = id
+evalAst :: MalValue -> Either String MalValue
+evalAst (MalSym sym) =
+  case M.lookup sym env of
+    Just val -> Right $ MalFunc val
+    Nothing -> Left $ "couldn't find symbol '" ++ (T.unpack sym) ++ "'"
+evalAst (MalList vals) =
+  case (flattenEithers $ map malEval vals) of
+    Left (err :| _) -> Left err
+    Right evalVals -> Right $ MalList $ evalVals
+evalAst val = Right val
 
-malPrint :: MalValue -> Text
+malEval :: MalValue -> Either String MalValue
+malEval (MalList []) = Right $ MalList []
+malEval l@(MalList vals) =
+  case evalAst l of
+    Left err -> Left err
+    Right (MalList (MalFunc func : args)) -> Right $ func $ MalList args
+    wrongVal -> Left $ "expected function, got " ++ show wrongVal
+malEval mv = evalAst mv
+
+malPrint :: MalValue -> T.Text
 malPrint = printStr
 
-rep :: Text -> Text
+rep :: T.Text -> T.Text
 rep str =
-    case malRead str of
-        Left err -> pack err
-        Right mv -> (malPrint . malEval) mv
+  case malRead str of
+    Left err -> T.pack err
+    Right mv ->
+      case malEval mv of
+        Left err -> T.pack err
+        Right result -> malPrint result
 
 repl :: IO ()
 repl = do
-    putStr "user> "
-    hFlush stdout
+  putStr "user> "
+  hFlush stdout
 
-    TIO.getLine >>= (TIO.putStrLn . rep)
+  TIO.getLine >>= (TIO.putStrLn . rep)
 
-    repl
+  repl
 
 
 main :: IO ()
