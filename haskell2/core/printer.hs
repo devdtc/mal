9fa894b853c1054a66b2ba0b128b1c0bc65ba432
Dan <devdtc@gmail.com>
2018-02-23 20:00:10 -0500

    Move printing to types.hs to implement Show on MalValue

--- haskell2/core/printer.hs
+++ haskell2/core/printer.hs
@@ -10,8 +10,5 @@ import qualified Data.Text as T
 
 
 printStr :: MalValue -> T.Text
-printStr (MalList vals) = T.concat [T.pack "(", T.intercalate (T.pack " ") (map printStr vals), T.pack ")"]
-printStr (MalSym sym) = sym
-printStr (MalInt int) = T.pack $ show int
-printStr (MalStr str) = T.concat [T.pack "\"", str, T.pack "\""]
+printStr = T.pack . show
 
--- haskell2/core/types.hs
+++ haskell2/core/types.hs
@@ -1,16 +1,26 @@
 
 module Types (
+    MalFunc,
     MalValue(..)
 )
 where
 
-import Data.Text
+import qualified Data.Text as T
+import Data.List
 
+type MalFunc = MalValue -> MalValue
 
 data MalValue
     = MalList [MalValue]
-    | MalSym Text
+    | MalSym T.Text
     | MalInt Int
-    | MalStr Text
-    deriving (Show)
+    | MalStr T.Text
+    | MalFunc MalFunc
+
+instance Show MalValue where
+    show (MalList vals) = concat ["(", intercalate " " (map show vals), ")"]
+    show (MalSym sym) = T.unpack sym
+    show (MalInt int) = show int
+    show (MalStr str) = concat ["\"", T.unpack str, "\""]
+    show (MalFunc _) = "[FUNCTION]"
 
