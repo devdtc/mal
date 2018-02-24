293d56ac32b8b92e6b4a3121b78ee83a1f0283f8
Dan <devdtc@gmail.com>
2018-02-24 00:01:15 -0500

    Complete step 2 (soft)

--- haskell2/step2_eval.hs
+++ haskell2/step2_eval.hs
@@ -4,7 +4,6 @@ import Reader
 import Printer
 
 import Data.Either
-import Data.List.NonEmpty (NonEmpty(..))
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
@@ -12,19 +11,46 @@ import qualified Data.Text.IO as TIO
 import System.IO
 
 
-env :: M.Map T.Text MalFunc
-env = M.fromList
-  [ (T.pack "+", \(MalList [MalInt x, MalInt y]) -> MalInt (x + y))
-  , (T.pack "-", \(MalList [MalInt x, MalInt y]) -> MalInt (x - y))
-  , (T.pack "*", \(MalList [MalInt x, MalInt y]) -> MalInt (x * y))
-  , (T.pack "/", \(MalList [MalInt x, MalInt y]) -> MalInt (x `div` y))
-  ]
+leftOrRights :: [Either a b] -> Either a [b]
+leftOrRights xs =
+  if null ls
+    then Right rs
+    else Left $ head ls
+ where
+  (ls, rs) = partitionEithers xs
 
-flattenEithers :: [Either a b] -> Either (NonEmpty a) [b]
-flattenEithers es =
-  case partitionEithers es of
-    (    [], rs) -> Right rs
-    (l : ls, _ ) -> Left $ l :| ls
+
+simpleOp :: [MalValue -> MalResult] -> ([MalValue] -> MalValue) -> MalFunc
+simpleOp argChecks op = \args ->
+  let
+    nargs = length args
+    nchecks = length argChecks
+  in
+    if nargs < nchecks
+      then Left "too few arguments provided"
+      else if nargs > nchecks
+        then Left "too many arguments provided"
+        else
+          let checked = zipWith ($) argChecks args
+          in op <$> leftOrRights checked
+
+expectInt :: MalValue -> MalResult
+expectInt int@(MalInt _) = Right int
+expectInt wrongVal = Left $ show "expected int, got " ++ show wrongVal
+
+envFuncs :: [(String, [MalValue] -> Either String MalValue)]
+envFuncs =
+  [ ("+", binaryIntOp $ \x y -> (x + y))
+  , ("-", binaryIntOp $ \x y -> (x - y))
+  , ("*", binaryIntOp $ \x y -> (x * y))
+  , ("/", binaryIntOp $ \x y -> (x `div` y))
+  ]
+ where
+  binaryIntOp op = simpleOp [expectInt, expectInt] $ \[MalInt x, MalInt y] -> MalInt $ op x y
+
+env :: M.Map T.Text MalFunc
+env = M.fromList $ map (\(s, f) -> (T.pack s, f)) envFuncs
+
 
 malRead :: T.Text -> Either String MalValue
 malRead = readStr
@@ -32,34 +58,30 @@ malRead = readStr
 evalAst :: MalValue -> Either String MalValue
 evalAst (MalSym sym) =
   case M.lookup sym env of
-    Just val -> Right $ MalFunc val
+    Just symVal -> Right $ MalFunc symVal
     Nothing -> Left $ "couldn't find symbol '" ++ (T.unpack sym) ++ "'"
 evalAst (MalList vals) =
-  case (flattenEithers $ map malEval vals) of
-    Left (err :| _) -> Left err
-    Right evalVals -> Right $ MalList $ evalVals
+  let evalVals = leftOrRights $ map malEval vals
+  in MalList <$> evalVals
 evalAst val = Right val
 
 malEval :: MalValue -> Either String MalValue
-malEval (MalList []) = Right $ MalList []
-malEval l@(MalList vals) =
-  case evalAst l of
+malEval list@(MalList []) = Right list
+malEval list@(MalList  _) =
+  case evalAst list of
     Left err -> Left err
-    Right (MalList (MalFunc func : args)) -> Right $ func $ MalList args
-    wrongVal -> Left $ "expected function, got " ++ show wrongVal
+    Right (MalList (MalFunc func : args)) -> func args
+    Right (MalList (wrongVal : _)) -> Left $ "expected function, got " ++ show wrongVal
+    Right _ -> error "evalAst returned non-list from list"
 malEval mv = evalAst mv
 
 malPrint :: MalValue -> T.Text
 malPrint = printStr
 
 rep :: T.Text -> T.Text
-rep str =
-  case malRead str of
-    Left err -> T.pack err
-    Right mv ->
-      case malEval mv of
-        Left err -> T.pack err
-        Right result -> malPrint result
+rep str = either T.pack malPrint result
+ where
+  result = malRead str >>= malEval
 
 repl :: IO ()
 repl = do
